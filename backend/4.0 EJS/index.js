eda644edb0196c2a3fa1e2683e9c2d3ce98221f2
Henry <henrygl450@yahoo.com>
2024-07-30 11:37:21 -0400

    update

---
+++ backend/4.0 EJS/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res)=>{
+    const d = new Date();
+    let day = d.getDay();
+
+
+    let type = "a weekday";
+    let adv = "it's time to work hard";
+
+    if (day === 0 || day === 6){
+        type = "a weekend";
+        adv = "it's time to have fun";
+    }
+
+    res.render("index.ejs", 
+    {dayType: type, 
+    advice: adv});
+})
+
+app.listen(port, ()=>{
+    console.log(`server runnning on port ${port}`);
+})
+
+
---
+++ backend/4.2/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  let fname = req.body['fName'] || '';
+  let lname = req.body['lName'] || '';
+
+  let count = fname.length + lname.length;
+
+  res.render("index.ejs", {numLetters: count});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
